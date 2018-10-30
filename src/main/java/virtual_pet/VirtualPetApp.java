82e5c9920b710a1b51e82e7f96a90d776240617b
Joshua Echt <jecht@kent.edu>
2018-10-30 00:24:13 -0400

    updated app notes

--- src/main/java/virtual_pet/VirtualPetApp.java
+++ src/main/java/virtual_pet/VirtualPetApp.java
@@ -6,7 +6,7 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println(
-				"You find a cute robot laying in a field.\nThat's odd, but you should probably take it home before it's \neaten by vultures or something.");
+				"You find an awesome robot laying in a field.\nThat's weird, but it should go home before it's \neaten by vultures or animals.");
 		System.out.println("Please name your robot:");
 		String givenName = input.nextLine();
 		VirtualPet robot = new VirtualPet(givenName);
@@ -57,30 +57,30 @@ public class VirtualPetApp {
 		// TODO add in other activities- go on walk, learn to play an instrument,
 		// discuss philosophy
 		else if (choice.equalsIgnoreCase("exit")) {
-			System.out.println("You abandon " + robot.getName());
+			System.out.println("You leave " + robot.getName());
 			System.exit(0);
 		} else {
-			System.out.println("Invalid choice. You ask your robot to mop the lawn. It does not comply.");
+			System.out.println("Bad choice. You ask your robot to mow the lawn. It fails to comply.");
 		}
 	}
 
 	public static String askActivity(Scanner input, VirtualPet robot) {
 		System.out.println("What would you like  to do with " + robot.getName() + "? Press a number key.");
-		System.out.println("1: Do Nothing");
+		System.out.println("1: Do nothing");
 		System.out.println("2: Charge " + robot.getName());
 		System.out.println("3: Feed " + robot.getName());
 		System.out.println("4: Garden together");
 		System.out.println("5: Watch TV together");
 		System.out.println("6: Teach " + robot.getName() + " to play an instrument.");
-		System.out.println("7: Go on a walk");
+		System.out.println("7: Go on a nice walk");
 		if (robot.getHacking() > 0) {
-			System.out.println("Type HACKING to ask your robot to stop hacking the mainframe.");
+			System.out.println("Type HACKING in all caps to ask your robot to stop hacking the computer system.");
 		}
 		if (robot.getVirus() && !robot.beenToDoctor) {
-			System.out.println("Type DOCTOR to take your robot to the doctor.");
+			System.out.println("Type DOCTOR in all caps to take your robot to the doctor.");
 		}
 		if (robot.getVirus() && robot.beenToDoctor) {
-			System.out.println("Type ENGINEER to take your robot to an engineer.");
+			System.out.println("Type ENGINEER in all caps to take your robot to an engineer.");
 		}
 
 		System.out.println("Type EXIT to abandon " + robot.getName() + " and quit application.");
